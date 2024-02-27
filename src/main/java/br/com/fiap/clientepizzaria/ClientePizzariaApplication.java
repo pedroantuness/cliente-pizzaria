e66be95716195665f4fe98b77ffa25ac8d332c55
pedroantuness <pedro280702@gmail.com>
2024-02-27 09:04:01 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/clientepizzaria/ClientePizzariaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.clientepizzaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientePizzariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientePizzariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/clientepizzaria/ClientePizzariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.clientepizzaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientePizzariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
